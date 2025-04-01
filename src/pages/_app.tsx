c9dc89f76112e66561d6dcd795123474e9d3104b
Karolis Grinkus <karolis@adaptagency.com>
2025-04-01 14:39:37 +0300

    boop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,34 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react';
+import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
-    <ChakraProvider theme={
-      {
-        styles: {
-          global: {
-            body: {
-              bg: 'gray.50',
+    <>
+      <ContentfulLivePreviewProvider
+        locale={router.locale || 'en-US'}
+        enableInspectorMode={pageProps.draftMode}
+        enableLiveUpdates={pageProps.draftMode}
+        debugMode={true}
+      >
+        <ChakraProvider theme={
+          {
+            styles: {
+              global: {
+                body: {
+                  bg: 'gray.50',
+                },
+              },
             },
-          },
-        },
-      }
-    }>
-      <Component {...pageProps} />
-    </ChakraProvider>
+          }
+        }>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ContentfulLivePreviewProvider>
+    </>
   );
 }
