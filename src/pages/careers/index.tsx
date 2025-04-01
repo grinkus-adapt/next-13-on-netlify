c1546447dd96a9fdec88ad2d199a915d6e5dc3fe
Karolis Grinkus <karolis@adaptagency.com>
2025-04-01 14:53:56 +0300

    boop

--- src/pages/careers/index.tsx
+++ src/pages/careers/index.tsx
@@ -1,3 +1,4 @@
+import type { GetServerSideProps } from 'next';
 import { useSearchParams } from "next/navigation"
 
 export default function Page() {
@@ -13,3 +14,32 @@ export default function Page() {
   <pre>searchParams.get(`q`): {JSON.stringify(searchParams.get(`q`))}</pre>
   </div>
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+  res,
+}) => {
+  // Extract relevant query parameters
+  const q = query.q as string;
+  const locationId = query.location as string;
+  const teamId = query.team as string;
+  const searchQuery = query.searchQuery ? (query.searchQuery as string) : null;
+
+  console.log(`q`, q);
+  console.log(`locationId`, locationId);
+  console.log(`teamId`, teamId);
+  console.log(`searchQuery`, searchQuery);
+
+  try {
+    res.setHeader('Netlify-Vary', 'query=team|location|searchQuery|pageNumber');
+
+    return {
+      props: {},
+    };
+  } catch (error) {
+    console.error('Failed to fetch job postings:', error);
+    return {
+      props: {},
+    };
+  }
+};
