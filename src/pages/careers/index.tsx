a4a460d7752d8847618374a64173814ecbcf6028
Karolis Grinkus <karolis@adaptagency.com>
2025-04-01 10:17:22 +0300

    boop

--- src/pages/careers/index.tsx
+++ src/pages/careers/index.tsx
@@ -6,5 +6,10 @@ export default function Page() {
   console.log(`searchParams`, searchParams);
   console.log(`searchParams.get('q')`, searchParams.get('q'));
 
-  return <div>Careers page</div>
+  return <div>
+  <div>Careers page</div>
+  <pre>{JSON.stringify(searchParams, null, 2)}</pre>
+  <hr style={{margin: 50}}/>
+  <pre>searchParams.get(`q`): {JSON.stringify(searchParams.get(`q`))}</pre>
+  </div>
 }
